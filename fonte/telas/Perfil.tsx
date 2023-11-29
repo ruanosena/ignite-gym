2557b6f64b6023a1d1a87037d25d1b7d47c7a825
ruanosena <ruosena@hotmail.com>
2023-11-29 14:08:46 -0300

    galeria de imagens

--- fonte/telas/Perfil.tsx
+++ fonte/telas/Perfil.tsx
@@ -2,14 +2,53 @@ import Botao from "@comp/Botao";
 import Entrada from "@comp/Entrada";
 import TelaCabecalho from "@comp/TelaCabecalho";
 import UsuarioFoto from "@comp/UsuarioFoto";
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from "native-base";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
+import * as ImagemSelecionador from "expo-image-picker";
+import * as ArquivoSistema from "expo-file-system";
 
 const fotoTamanho = 33;
 
 export default function Perfil() {
 	const [fotoEstaCarregando, defFotoEstaCarregando] = useState(false);
+	const [usuarioFoto, defUsuarioFoto] = useState("https://github.com/ruanosena.png");
+
+	const torrada = useToast();
+
+	async function lidarUsuarioSelecionarFoto() {
+		defFotoEstaCarregando(true);
+		try {
+			const fotoSelecionada = await ImagemSelecionador.launchImageLibraryAsync({
+				mediaTypes: ImagemSelecionador.MediaTypeOptions.Images,
+				quality: 1,
+				aspect: [4, 4],
+				allowsEditing: true,
+				selectionLimit: 1,
+			});
+
+			if (fotoSelecionada.canceled) return;
+
+			if (fotoSelecionada.assets[0]) {
+				const fotoInfo = await ArquivoSistema.getInfoAsync(fotoSelecionada.assets[0].uri);
+
+				if (fotoInfo.exists && fotoInfo.size / 1024 / 1024 > 5) {
+					return torrada.show({
+						title: "Essa imagem é muito grande, escolha uma de até 5MB.",
+						placement: "top",
+						bgColor: "red.500",
+					});
+				}
+
+				defUsuarioFoto(fotoSelecionada.assets[0].uri);
+			}
+		} catch (erro) {
+			console.log(erro);
+		} finally {
+			defFotoEstaCarregando(false);
+		}
+	}
+
 	return (
 		<VStack flex={1}>
 			<TelaCabecalho>Perfil</TelaCabecalho>
@@ -24,13 +63,9 @@ export default function Perfil() {
 							endColor="gray.300"
 						/>
 					) : (
-						<UsuarioFoto
-							source={{ uri: "https://github.com/ruanosena.png" }}
-							alt="Foto do usuário"
-							tamanho={fotoTamanho}
-						/>
+						<UsuarioFoto source={{ uri: usuarioFoto }} alt="Foto do usuário" tamanho={fotoTamanho} />
 					)}
-					<TouchableOpacity>
+					<TouchableOpacity onPress={lidarUsuarioSelecionarFoto}>
 						<Text color="green.500" fontSize="md" fontWeight="bold" mt={2} mb={8}>
 							Alterar foto
 						</Text>
